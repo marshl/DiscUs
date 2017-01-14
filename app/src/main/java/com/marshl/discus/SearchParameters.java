054022b04b32cb629c36da7805ecbb05662344bf
marshl <marshl@live.com.au>
2017-01-14 17:43:14 +1100

    Added the SearchParameters class to move data from the search activity to the results activity

---
+++ app/src/main/java/com/marshl/discus/SearchParameters.java
@@ -0,0 +1,49 @@
+package com.marshl.discus;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Liam on 14/01/2017.
+ */
+
+public class SearchParameters implements Parcelable {
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public SearchParameters createFromParcel(Parcel in) {
+            return new SearchParameters(in);
+        }
+
+        public SearchParameters[] newArray(int size) {
+            return new SearchParameters[size];
+        }
+    };
+    private String searchText;
+
+    public SearchParameters() {
+        super();
+    }
+
+    public SearchParameters(Parcel in) {
+        super();
+        this.searchText = in.readString();
+    }
+
+    public String getSearchText() {
+        return this.searchText;
+    }
+
+    public void setSearchText(String searchText) {
+        this.searchText = searchText;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(this.searchText);
+    }
+}
