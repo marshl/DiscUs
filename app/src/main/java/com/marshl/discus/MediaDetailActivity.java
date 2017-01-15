754dbd54f996117d9a9a2ea1ffb8daf7e9823336
marshl <marshl@live.com.au>
2017-01-15 20:50:09 +1100

    Removed the non-functioning back button from the media details page

--- app/src/main/java/com/marshl/discus/MediaDetailActivity.java
+++ app/src/main/java/com/marshl/discus/MediaDetailActivity.java
@@ -15,16 +15,6 @@ public class MediaDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_media_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
