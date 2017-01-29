74ea0c396e8c266d47158e6a79b508a90055cada
marshl <marshl@live.com.au>
2017-01-29 23:05:11 +1100

    Added more fields to the media details screen

--- app/src/main/java/com/marshl/discus/MediaDetailActivity.java
+++ app/src/main/java/com/marshl/discus/MediaDetailActivity.java
@@ -1,8 +1,10 @@
 package com.marshl.discus;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -27,12 +29,34 @@ public class MediaDetailActivity extends AppCompatActivity {
 
         TextView releaseDateView = (TextView) this.findViewById(R.id.media_release_date);
         if (this.media.getReleaseDate() != null) {
-            DateFormat formatter = DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault());
+            DateFormat formatter = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault());
             releaseDateView.setText(formatter.format(this.media.getReleaseDate()));
         } else {
             TextView releaseDateLabel = (TextView)this.findViewById(R.id.media_release_label);
             releaseDateLabel.setVisibility(View.GONE);
             releaseDateView.setVisibility(View.GONE);
         }
+
+        TextView contentRatingView = (TextView)this.findViewById(R.id.media_content_rating);
+        contentRatingView.setText(this.media.getContentRating());
+
+        TextView genreView = (TextView)this.findViewById(R.id.media_genre);
+        genreView.setText(this.media.getGenres());
+
+        TextView plotView = (TextView)this.findViewById(R.id.media_plot);
+        plotView.setText(this.media.getPlot());
+
+        TextView directorView = (TextView)this.findViewById(R.id.media_director);
+        directorView.setText(this.media.getDirector());
+
+        TextView writerView = (TextView)this.findViewById(R.id.media_writer);
+        writerView.setText(this.media.getWriter());
+
+        TextView metascoreView = (TextView)this.findViewById(R.id.media_metascore);
+        metascoreView.setText(String.format(Locale.getDefault(), "%d", this.media.getMetascore()));
+
+        int metascoreColor = MetascoreUtils.getMetascoreColor(this.media.getMetascore(), this.media.getType() != null && this.media.getType().equals("game"));
+        metascoreView.setBackgroundColor(metascoreColor);
+        Log.d("COLOR", "Score: " + Integer.toString(this.media.getMetascore()) + " " + String.format(Locale.getDefault(), "%d", this.media.getMetascore()));
     }
 }
