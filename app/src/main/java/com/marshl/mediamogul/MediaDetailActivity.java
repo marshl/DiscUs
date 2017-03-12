76e90073e46c744440ff1537444bdd6f76a4df69
marshl <marshl@live.com.au>
2017-03-12 22:03:50 +1100

    Changed the detail activity to change the text and color of the library/wishlist buttons depending on ownership state

--- app/src/main/java/com/marshl/mediamogul/MediaDetailActivity.java
+++ app/src/main/java/com/marshl/mediamogul/MediaDetailActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.URLSpan;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,6 +31,9 @@ public class MediaDetailActivity extends AppCompatActivity {
 
     private ParcelableMedia media;
 
+    private Button wishlistButton;
+    private Button libraryButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +45,9 @@ public class MediaDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_media_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        this.wishlistButton = (Button) this.findViewById(R.id.media_wishlist_button);
+        this.libraryButton = (Button) this.findViewById(R.id.media_library_button);
 
         TextView mediaTitleView = (TextView) this.findViewById(R.id.media_details_title);
         mediaTitleView.setText(this.media.getTitle());
@@ -125,6 +133,8 @@ public class MediaDetailActivity extends AppCompatActivity {
             View imdbContainer = this.findViewById(R.id.media_imdb_container);
             imdbContainer.setVisibility(View.GONE);
         }
+
+        this.refreshButtonLayout();
     }
 
     private SpannableString getUrlLinkText(CharSequence text) {
@@ -143,9 +153,33 @@ public class MediaDetailActivity extends AppCompatActivity {
     }
 
     public void onAddToLibraryClick(View view) {
+        if (this.media.getOwnershipStatus() == Media.OwnershipType.OWNED) {
+            this.media.setOwnershipStatus(Media.OwnershipType.NOT_OWNED);
+        } else {
+            this.media.setOwnershipStatus(Media.OwnershipType.OWNED);
+        }
 
-        this.media.setOwnershipStatus(Media.OwnershipType.OWNED);
+        this.insertIntoDatabase();
+        this.refreshButtonLayout();
+    }
 
+    public void onAddToWishlistClick(View view) {
+
+        if (this.media.getOwnershipStatus() == Media.OwnershipType.OWNED) {
+            return;
+        }
+
+        if (this.media.getOwnershipStatus() == Media.OwnershipType.ON_WISHLIST) {
+            this.media.setOwnershipStatus(Media.OwnershipType.NOT_OWNED);
+        } else {
+            this.media.setOwnershipStatus(Media.OwnershipType.ON_WISHLIST);
+        }
+        this.insertIntoDatabase();
+        this.refreshButtonLayout();
+    }
+
+
+    private void insertIntoDatabase() {
         MediaReaderDbHelper dbHelper = new MediaReaderDbHelper(this);
         try {
             dbHelper.insertMediaRecord(this.media);
@@ -154,6 +188,36 @@ public class MediaDetailActivity extends AppCompatActivity {
                     "An error occurred when adding the media: " + ex.getMessage(),
                     Toast.LENGTH_LONG);
             toast.show();
+        }
+    }
+
+    private void refreshButtonLayout() {
+
+        final Resources res = this.getResources();
+
+        switch (this.media.getOwnershipStatus()) {
+            case OWNED:
+                this.libraryButton.setBackgroundColor(ResourcesCompat.getColor(res, android.R.color.holo_blue_dark, null));
+                this.libraryButton.setText(res.getString(R.string.remove_from_library));
+
+                this.wishlistButton.setBackgroundColor(ResourcesCompat.getColor(res, android.R.color.background_light, null));
+                this.wishlistButton.setText(res.getString(R.string.add_to_wishlist));
+                this.wishlistButton.setEnabled(false);
+                break;
+            case NOT_OWNED:
+                this.libraryButton.setBackgroundColor(ResourcesCompat.getColor(res, android.R.color.background_light, null));
+                this.libraryButton.setText(res.getString(R.string.add_to_library));
+                this.wishlistButton.setBackgroundColor(ResourcesCompat.getColor(res, android.R.color.background_light, null));
+                this.wishlistButton.setText(res.getString(R.string.add_to_wishlist));
+                this.wishlistButton.setEnabled(true);
+                break;
+            case ON_WISHLIST:
+                this.libraryButton.setBackgroundColor(ResourcesCompat.getColor(res, android.R.color.background_light, null));
+                this.libraryButton.setText(res.getString(R.string.add_to_library));
+                this.wishlistButton.setBackgroundColor(ResourcesCompat.getColor(res, android.R.color.holo_blue_dark, null));
+                this.wishlistButton.setText(res.getString(R.string.remove_from_wishlist));
+                this.wishlistButton.setEnabled(true);
+                break;
         }
     }
 }
