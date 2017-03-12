79c2666b62c806e88418b4ce0c6fef07dd1d4118
marshl <marshl@live.com.au>
2017-03-12 20:43:29 +1100

    Added a link to the IMDb and Metacritic websites from the media detail activity

--- app/src/main/java/com/marshl/mediamogul/MediaDetailActivity.java
+++ app/src/main/java/com/marshl/mediamogul/MediaDetailActivity.java
@@ -1,14 +1,20 @@
 package com.marshl.mediamogul;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.URLSpan;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.util.Locale;
@@ -17,6 +23,8 @@ public class MediaDetailActivity extends AppCompatActivity {
 
     public static final String RESULT_INTENT_IMDB_ID = "IMDB_ID";
     public static final String RESULT_INTENT_OWNERSHIP = "OWNERSHIP";
+
+    private static final String URL_ENCODING = "utf-8";
 
     private ParcelableMedia media;
 
@@ -58,17 +66,48 @@ public class MediaDetailActivity extends AppCompatActivity {
         TextView writerView = (TextView) this.findViewById(R.id.media_writer);
         writerView.setText(this.media.getWriter());
 
-
         if (media.getMetascore() != null && media.getMetascore() != 0) {
-            TextView metascoreView = (TextView) this.findViewById(R.id.media_metascore);
-            LinearLayout metascoreWrapper = (LinearLayout) this.findViewById(R.id.media_metascore_wrapper);
+            final TextView metascoreView = (TextView) this.findViewById(R.id.media_metascore);
+            final LinearLayout metascoreWrapper = (LinearLayout) this.findViewById(R.id.media_metascore_wrapper);
 
             metascoreView.setText(String.format(Locale.getDefault(), "%d", this.media.getMetascore()));
-            int metascoreColor = MetascoreUtils.getMetascoreColor(this.media.getMetascore(), this.media.isGame());
-            int metascoreTextColor = MetascoreUtils.getMetascoreTextColor(this.media.getMetascore(), this.media.isGame());
+            final int metascoreColor = MetascoreUtils.getMetascoreColor(this.media.getMetascore(), this.media.isGame());
+            final int metascoreTextColor = MetascoreUtils.getMetascoreTextColor(this.media.getMetascore(), this.media.isGame());
 
             metascoreWrapper.setBackgroundColor(metascoreColor);
             metascoreView.setTextColor(metascoreTextColor);
+
+            final TextView metacriticLink = (TextView) this.findViewById(R.id.media_metacritic_link);
+            metacriticLink.setText(this.getUrlLinkText(metacriticLink.getText()), TextView.BufferType.SPANNABLE);
+
+            metacriticLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.metacritic.com/search/all/" + URLEncoder.encode(media.getTitle(), URL_ENCODING) + "/results"));
+                        startActivity(browserIntent);
+                    } catch (UnsupportedEncodingException ex) {
+
+                    }
+                }
+            });
+
+            final TextView imdbLink = (TextView) this.findViewById(R.id.media_imdb_link);
+            imdbLink.setText(this.getUrlLinkText(imdbLink.getText()), TextView.BufferType.SPANNABLE);
+
+            imdbLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.imdb.com/title/" + URLEncoder.encode(media.getImdbId(), URL_ENCODING) + "/"));
+                        startActivity(browserIntent);
+                    } catch (UnsupportedEncodingException ex) {
+
+                    }
+                }
+            });
+
+
         } else {
             LinearLayout metascoreContainer = (LinearLayout) findViewById(R.id.media_metascore_container);
             metascoreContainer.setVisibility(View.GONE);
@@ -83,6 +122,12 @@ public class MediaDetailActivity extends AppCompatActivity {
             View imdbContainer = this.findViewById(R.id.media_imdb_container);
             imdbContainer.setVisibility(View.GONE);
         }
+    }
+
+    private SpannableString getUrlLinkText(CharSequence text) {
+        final SpannableString spannableString = new SpannableString(text);
+        spannableString.setSpan(new URLSpan(""), 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return spannableString;
     }
 
     @Override
